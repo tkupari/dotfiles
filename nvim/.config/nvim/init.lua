de323bf8ced1e42314001ea3e74c39b5831f3086
Teemu Kupari <tkupari@gmail.com>
2023-01-23 10:15:17 +0200

    nvim: enable buffer completion

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -28,7 +28,7 @@ require('packer').startup(function(use)
 
   use { -- Autocompletion
     'hrsh7th/nvim-cmp',
-    requires = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
+    requires = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip', 'hrsh7th/cmp-buffer' },
   }
 
   use { -- Highlight, edit, and navigate code
@@ -424,6 +424,7 @@ cmp.setup {
   sources = {
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
+    { name = 'buffer' },
   },
 }
 
